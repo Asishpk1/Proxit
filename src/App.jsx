0fce84e77fef457d0b20d92d753398e0fb7a33a1
Asishpk1 <asishpkrishna@gmail.com>
2026-04-22 22:45:10 +0530

    Proxit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import bg from './assets/bg.jpg'
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover",backgroundPosition: "center",minHeight: "100vh" }}>
+     
+      <div className="header d-flex justify-content-center">
+        <img src={logo} alt="" className='p-1 w-25' />
+      </div>
+
+      <div className='mt-5 pt-5 d-flex flex-column justify-content-center align-items-center'>
+        <h6 className='p-3 rounded' style={{backgroundColor:'rgb(223, 220, 8)', fontWeight:'bold'}}>JOIN FREE WHATSAPP CHANNEL</h6>
+        <h2 className='text-light mt-3'>PROXIT WAYS</h2>
+        <p>👇👇👇👇👇</p>
+        <p className='text-light'>JOIN FREE WHATSAPP</p>
+        <span className='text-light'>Get <span className='bg-success p-1 pb-2 px-2 rounded'>high-accuracy</span> signals for</span>
+        <p className='mt-4 text-light'><span className='bg-primary text-light p-1 rounded'>NIFTY</span> I <span className=' text-light p-1 rounded' style={{backgroundColor:'rgb(10, 109, 30)'}}>BANKNIFTY</span> I <span className=' text-light p-1 rounded' style={{backgroundColor:'rgb(144, 4, 99)'}}>SENSEX</span></p>
+        <span className='text-light p-1 rounded'style={{backgroundColor:'rgb(255, 0, 157)'}} >STOCK OPTION</span>
+        <p className='mt-4 text-light'><span className=' text-light p-1 rounded' style={{backgroundColor:'rgb(28, 3, 142)'}}>FOREX</span> I <span className=' text-dark p-1 rounded' style={{backgroundColor:'rgb(223, 220, 8)'}}>GOLD (XAUUSD)</span> I <span className=' text-light p-1 rounded' style={{backgroundColor:'rgb(251, 108, 6)'}}>BTCUSD</span></p>
+        <p className='text-light'>📈 95% Success Rate</p>
+        <p className='text-light'>🔥 Join the Best in Global Forex Trading 💼</p>
+        <p className='text-light'>✅ Daily 2-8 Signals </p>
+        <p className='text-light'>✅ 24x7  Live Support </p>
+        <p className='text-light'>👇 Click the button below 👇</p>
+        <button className='btn W-25 mt-sm-3' style={{backgroundColor:'rgb(223, 220, 8)', fontWeight:'bold'}}> JOIN FREE WHATSAPP CHANNEL</button>
+      </div>
+      </div> 
+    </>
+  )
+}
+
+export default App
